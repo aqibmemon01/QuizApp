98cfa5578fecb4eeffe3699d247858076ac4b514
Abu Bakar <aqibmemon786@gmail.com>
2021-01-20 21:03:40 +0500

    Random Answer Function Add

--- src/Services/quiz_service.ts
+++ src/Services/quiz_service.ts
@@ -1,16 +1,19 @@
 import {questionType, Quiz} from '../Types/quiz_Types'
 
+const shuffleArray = (arr:any[])=>
+  [...arr].sort(()=>Math.random()-0.5)
 
 export const getQuizDetails = async(totalQuestion:number, level:string): Promise<Quiz[]> => {
 
   const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestion}&difficulty=${level}&type=multiple`)
   let {results} = await res.json() 
-    
+
+
   const quiz = results.map((questionObj: Quiz)=>{
     return{
       question:questionObj.question,
       answer:questionObj.correct_answer,
-      option:questionObj.incorrect_answers.concat(questionObj.correct_answer)
+      option:shuffleArray(questionObj.incorrect_answers.concat(questionObj.correct_answer))
     }
   })
   return quiz

71c62f0ae4a4fabf7e0c2c42806ac4f0a487e2f4
AqibMemon <maqibmemon786@gmail.com>
2021-02-13 14:57:02 -0800

    Work On Result Page

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,19 +3,24 @@ import logo from './logo.svg';
 import './QuizCard.css';
 import {getQuizDetails} from './Services/quiz_service';
 import {questionType,Quiz} from './Types/quiz_Types';
-import QuestionCard from './Components/QuestionCard'
+import QuestionCard from './Components/QuestionCard';
+import Trophy from './trophy.svg';
+import Fail from './fail.svg';
+
+
 
 function App(props:any) {
 
 let [quiz, setquiz] = useState<questionType[]>([]);
 let [CurrentStep, setCurrentStep] = useState(0);
 let [CurrentScore, setCurrentScore] = useState(0);
+let [Result, setResult] = useState(-1);
 
 
  useEffect(()=>{
 
    async function getData() {
-     const questions = await getQuizDetails(15,"easy",props.QuizType)
+     const questions = await getQuizDetails(10,"easy",props.QuizType)
      
       setquiz(questions)
        console.log(questions)
@@ -35,7 +40,10 @@ const handleSubmit = (e:React.FormEvent<EventTarget>,Ans:string) =>{
      }
   }
   else{
-    alert("Completed")
+    if(Ans===quiz[(CurrentStep)].answer){
+      setCurrentScore(++CurrentScore)
+    }
+    setResult(CurrentScore)
   }
 }
 
@@ -43,10 +51,10 @@ const handleSubmit = (e:React.FormEvent<EventTarget>,Ans:string) =>{
 if(!quiz.length){
   return(<h3 style={{textAlign:"center", marginTop:"250px", color:"white"}} >Loadingg</h3>)
 }
-console.log(quiz[0])
+
   return (
   <div className="CardMain">
-    <div>
+    <div className={Result==-1 ? "ShowMe" : "HideMe"} >
     <h3 className="ScoreBoard">Score {CurrentScore}</h3>
     <h5 style={{textAlign:"center"}} >{CurrentStep+1 + " of " + quiz.length}</h5>
    <QuestionCard 
@@ -55,6 +63,16 @@ console.log(quiz[0])
     callBack={handleSubmit}
    />
 </div>
+
+<div className={Result!=-1 ? "ResultDiv" : "HideMe"} >
+<img src={Result >= 5 ? Trophy : Fail} />
+<h2>{Result >= 5 ? "Congrats!" : "Sorry Fail"}</h2>
+<h1 style={{color:Result >= 5 ? "rgb(9, 146, 9)" : "red"}}>{((Result/quiz.length)*100).toFixed(1)}% Score</h1>
+<p  >You attempt<span> {quiz.length } Questions</span> and from that <span>{Result} answer</span> is correct</p>
+{/* color: rgb(9, 146, 9); */}
+
+</div>
+
   </div>
   );
 }
